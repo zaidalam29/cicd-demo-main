44ba28e5926fd25622448180b752d9ac625ca2fa
zaidalam29 <zaidalam29@gmail.com>
2025-07-14 13:41:41 +0530

    Changes Last

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Zaid Alam CI CD Repoitory\n');
+  res.end('Zaid Alam CI/CD Repoitory\n');
 });
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
-});
+});           
