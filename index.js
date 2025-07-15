2ac09c494a26167003268a469ca73af0ffe6f366
zaidalam29 <zaidalam29@gmail.com>
2025-07-15 14:57:49 +0530

    changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Zaid Alam - Full Stack Developer\n');
+  res.end('Zaid Alam - Full Stack Developer at Routa Digital India Pvt. Ltd.\n');
 });
 
 server.listen(port, () => {
